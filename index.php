1e01e71c5c843724754cf5b2f0f11167be5cad9a
Luciano Rangel Aguiar <luciano.rangel.aguiar@gmail.com>
2024-01-10 09:35:12 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <link rel="stylesheet" href="css\reset.css">
+  <link rel="stylesheet" href="css\styles2.css">
+</head>
+
+<body>
+
+  <div class="Content">
+
+    <div class="NavBar">
+
+      <div class="SmallLogo">
+
+        <img src="images\simple_logo.png" alt="">
+
+      </div>
+
+      <div class="Menu">
+
+        <a class="Button" href="#Start">
+          Inicio
+        </a>
+
+        <a class="Button" href="#Services">
+          Servicos
+        </a>
+
+        <div class="Socials">
+
+          <a href="https://www.instagram.com/urbandesignconstrucoes/" target="blank">
+            <ion-icon class="Icon" name="logo-instagram"></ion-icon>
+          </a>
+
+          <a href="https://wa.me/5522999753625" target="blank">
+            <ion-icon class="Icon" name="logo-whatsapp"></ion-icon>
+          </a>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div id="Start">
+
+
+      <div class="Logo">
+        <video disablePictureInPicture="true" controls disablePictureInPicture autoplay muted loop id="StartVideo">
+          <source src="images\baner-video.mp4" type="video/mp4">
+        </video>
+
+        <img src="images\full_logo.png" alt="">
+      </div>
+
+      <div class="Text">
+
+        <h1> Bem vindo a Urban Design! </h1>
+
+        <p>
+          Especialistas em arquitetura e urbanismo, oferecemos consultoria premium na execução de obras, com foco em qualidade e inovação, transformamos ideias em realidade. 
+          Desde projetos residenciais até empreendimentos urbanos, nossa equipe comprometida garante resultados excepcionais. Confie na Urban Design para a realização de seus sonhos.
+        </p>
+
+      </div>
+
+    </div>
+
+    <div id="Services">
+
+      <div class="BackgorundNav">
+
+      </div>
+
+      <div class="ServicesList">
+
+        <a class="Button" href="#arq">
+
+          Projetos arquitetônicos
+
+        </a>
+        <a class="Button" href="#urb">
+
+          Projetos urbanísticos
+
+        </a>
+
+      </div>
+      <div class="Galery">
+
+        <div class="ServiceCard" id="arq">
+
+          <div class="CardText">
+            <h1>Projetos arquitetônicos</h1>
+            <p>
+              O serviço de projetos arquitetônicos transforma concepções em realidade, integrando estética e funcionalidade. Profissionais colaboram estreitamente com clientes para criar espaços visualmente impactantes, utilizando ferramentas avançadas de modelagem a fim de entregar documentação detalhada orienta a execução da construção, garantindo a materialização eficiente do projeto.
+            </p>
+          </div>
+
+          <ul class="Carosel">
+            <li class="Img"><img src="images\fachada_01.jpg" alt=""></li>
+            <li class="Img"><img src="images\fachada_02.jpg" alt=""></li>
+            <li class="Img"><img src="images\fachada_03.jpg" alt=""></li>
+            <li class="Img"><img src="images\fachada_04.png" alt=""></li>
+            <li class="Img"><img src="images\planta-1.png" alt=""></li>
+            <li class="Img"><img src="images\planta-2.png" alt=""></li>
+            <li class="Img"><img src="images\planta-3.png" alt=""></li>
+            <li class="Img"><img src="images\planta-4.png" alt=""></li>
+          </ul>
+
+        </div>
+
+        <div class="ServiceCard" id="urb">
+
+          <div class="CardText">
+            <h1>Projetos urbanísticos</h1>
+            <p>
+              O serviço de projetos urbanísticos transformam áreas urbanas, integrando infraestrutura, espaços verdes e mobilidade. Profissionais colaboram para criar ambientes funcionais e esteticamente agradáveis, utilizando ferramentas avançadas a fim de entregar documentação detalhada orienta a execução eficaz do projeto urbano.
+            </p>
+          </div>
+
+          <ul class="Carosel">
+            <li class="Img"><img src="images\condominio-01.jpg" alt=""></li>
+            <li class="Img"><img src="images\condominio-02.png" alt=""></li>
+            <li class="Img"><img src="images\condominio-03.jpeg" alt=""></li>
+            <li class="Img"><img src="images\condominio-04.jpeg" alt=""></li>
+            <li class="Img"><img src="images\condominio-05.png" alt=""></li>
+            <li class="Img"><img src="images\condominio-06.png" alt=""></li>
+            <li class="Img"><img src="images\condominio-07.jpeg" alt=""></li>
+          </ul>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+
+
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
